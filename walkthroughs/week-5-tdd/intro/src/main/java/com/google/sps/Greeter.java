f4b5945fad451b1a395f2720df47b0ef13076ab6
Shivani Ghanta <shivani.ghanta@gmail.com>
2020-06-16 21:37:29 +0000

    modified example tests

--- walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
+++ walkthroughs/week-5-tdd/intro/src/main/java/com/google/sps/Greeter.java
@@ -22,6 +22,7 @@ public class Greeter {
    * Returns a greeting for the given name.
    */
   public String greet(String name) {
-    return "Hello " + name;
+    String greeting = "Hello " + name.trim();
+    return greeting;
   }
 }
--- walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-5-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -15,9 +15,45 @@
 package com.google.sps;
 
 import java.util.Collection;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 
 public final class FindMeetingQuery {
   public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
+      Collection<String> attendees = request.getAttendees();
+      int duration = (int) request.getDuration();
+
+      // if there are no attendees return whole day
+      if (attendees.isEmpty()) {
+          return Arrays.asList(TimeRange.WHOLE_DAY);
+      }
+
+      // if duration is too long return no options
+      if (duration > TimeRange.WHOLE_DAY.duration()) {
+          return Arrays.asList();
+      }
+
+      // find all the unique conflicting time ranges 
+      // go through all the events
+        // add each event "when" to conflictingTimes list
+        Set<TimeRange> conflictTimes = new HashSet<TimeRange>();
+        
+        for (Event event : Events) {
+            if (attendees.contains(event.getAttendees())) {
+
+            }
+        } 
+
+
+
+
+
+
+      return Arrays.asList(TimeRange.WHOLE_DAY);
   }
 }
